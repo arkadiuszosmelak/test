2230e15ce890eae8c68e1d8ef4be4b2474bedfc1
Gość <arkadiusz.osmelak@smcebi.edu.pl>
2018-10-29 15:25:34 +0100

    zmiana w python

--- plik.py
+++ plik.py
@@ -2,8 +2,8 @@ x = raw_input("Wpisz x: ")
 d = int(x)
 if d>=0:
     y = int(x)
-    print "E("+str(x)+")="+str(y)
+    print "E("+str(x)+")="+"hello"
 else:
     y = int(x)
     y -= 1
-    print "E("+str(x)+")="+str(y)
+    print "E("+str(x+1)+")="+str(y)
